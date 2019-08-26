d94e8fcf98ae2f0d95525a83c6b969d3d4f0bdcd
Debbie <debbiec73@yahoo.com>
2019-08-25 21:31:48 -0500

    js

---
+++ index.js
@@ -0,0 +1,29 @@
+$(function(){
+$('#js-shopping-list-form').submit(event => {
+    // stop the default form submission behavior
+    event.preventDefault();
+    const item = $('#shopping-list-entry').val();
+    $("#shopping-list-entry").val(" ");
+    $(".shopping-list").append(
+    `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+
+})
+});
+
+$(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+    $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+  });
+  
+  $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+    $(this).closest("li").remove();
+  });
